c63901e90156b7710dc4f2b15ef3ede593244502
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2020-10-08 15:32:58 -0400

    fisrt commit

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -10,3 +11,6 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+  // pexel api key: 563492ad6f9170000100000105e0d20fc08f4be7a2049f376bcb492a
