726f088af2b1ebc036f1627331152f47181bbd0e
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2020-10-11 17:56:52 -0400

    fix path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import { environment } from '../environments/environment';
     MatListModule,
     MatButtonModule,
     MatIconModule,
-    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     AppRequestCache,
